d6df03c0fc3b6fca0e6f4d7b3697ab75b6542bcb
Bryan Ross <git@rossipedia.com>
2017-07-19 13:24:28 -0600

    add umd builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,29 @@
 import path from 'path';
+import uglify from 'rollup-plugin-uglify';
 
-export default {
-  entry: path.resolve(__dirname, 'lib', 'index.js'),
-  dest: path.resolve(__dirname, 'fluent-validators.js'),
-  format: 'cjs',
-  moduleName: 'fluentValidators',
-  // sourceMap: true
-};
+export default [
+  {
+    entry: path.resolve(__dirname, 'lib', 'index.js'),
+    dest: path.resolve(__dirname, 'fluent-validators.js'),
+    format: 'cjs',
+  },
+  {
+    entry: path.resolve(__dirname, 'lib', 'index.js'),
+    dest: path.resolve(__dirname, 'fluent-validators.umd.js'),
+    format: 'umd',
+    moduleName: 'fluentValidators'
+  },
+  {
+    entry: path.resolve(__dirname, 'lib', 'index.js'),
+    dest: path.resolve(__dirname, 'fluent-validators.umd.min.js'),
+    format: 'umd',
+    moduleName: 'fluentValidators',
+    // sourceMap: true
+    plugins: [
+      uglify({
+        compress: true,
+        mangle: true,
+      }),
+    ],
+  },
+];
